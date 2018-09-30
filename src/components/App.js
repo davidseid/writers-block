8d9a580fe2c88f9c354b2d0f113c93dcb1f57815
David Seidenberg <davidseidenberg@gmail.com>
2018-09-30 11:51:57 -0700

    Refactor set source text logic to separate function

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Source from './Source';
 import Write from './Write';
 import Suggestions from './Suggestions';
+import setSourceText from '../actions/setSourceText';
 
 const title = 'Writer\'s Block';
 
@@ -16,48 +17,8 @@ class App extends React.Component {
   }
 
   setSourceText(text) {
-    const source = {};
-    const words = text.replace( /\n/g, " " ).split(' ');
-    let mostFrequentWordCount = 0;
 
-    
-    words.forEach((word, index) => {
-      word = word.toLowerCase();
-
-      if (index < words.length - 1) {
-        let nextWord = words[index + 1];
-        if (nextWord) {
-          nextWord = nextWord.toLowerCase();
-        }
-        if (word[word.length - 1] === '.' || 
-            word[word.length - 1] === ';' ||
-            word[word.length - 1] === ',') {
-              word = word.slice(0, word.length - 1);
-            }
-        if (nextWord[word.length - 1] === '.' || 
-            nextWord[word.length - 1] === ';' ||
-            nextWord[word.length - 1] === ',') {
-              nextWord = nextWord.slice(0, word.length - 1);
-            }
-        if (!source[word]) {
-          let nextWords = {};
-          nextWords[nextWord] = 1;
-          source[word] = {count: 1, nextWords: nextWords};
-          mostFrequentWordCount = 1;
-        } else {
-          source[word].count += 1;
-          if (source[word].count > mostFrequentWordCount) {
-            mostFrequentWordCount = source[word].count;
-            source['mostFrequentWord'] = word;
-          }
-          if (source[word].nextWords[nextWord]) {
-            source[word].nextWords[nextWord] += 1;
-          } else {
-            source[word].nextWords[nextWord] = 1;
-          }
-        }
-      }
-    });
+    const source = setSourceText(text);
 
     this.setState({
       source: source
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/components/index.js'
   ],
   module: {
     rules: [
